31001c3e614f9c875a90860872d41f7ec6443fc2
ianbautista <ianfbautista@gmail.com>
2020-09-07 16:43:42 -0700

    completed Order class setup

--- src/main/java/com/lambdaschool/ordersapp/models/Customer.java
+++ src/main/java/com/lambdaschool/ordersapp/models/Customer.java
@@ -1,4 +1,27 @@
 package com.lambdaschool.ordersapp.models;
 
-public class Customer {
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cusotmers")
+public class Customer
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private long custcode;
+
+    @Column(nullable = false)
+    private String custname;
+    private String custcity;
+    private String workingarea;
+    private String custcountry;
+    private String grade;
+    private double openingamt;
+    private double receiveamt;
+    private double paymentamt;
+    private double outstandingamt;
+    private String phone;
+
+
 }
--- src/main/java/com/lambdaschool/ordersapp/models/Order.java
+++ src/main/java/com/lambdaschool/ordersapp/models/Order.java
@@ -1,6 +1,77 @@
 package com.lambdaschool.ordersapp.models;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name ="orders")
 public class Order
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private long ordnum;
 
+    @Column(nullable = false)
+    private double ordamount;
+    private double advanceamount;
+    private String orderdescription;
+
+    // foreign key
+    @ManyToOne
+    @JoinColumn(name = "custcode", nullable = false)
+    private Customer customer;
+
+    // default constructor
+    public Order()
+    {
+    }
+    //constructor
+    public Order(double ordamount, double advanceamount, String orderdescription, Customer customer) {
+        this.ordamount = ordamount;
+        this.advanceamount = advanceamount;
+        this.orderdescription = orderdescription;
+        this.customer = customer;
+    }
+
+    //getters and setters
+    public long getOrdnum() {
+        return ordnum;
+    }
+
+    public void setOrdnum(long ordnum) {
+        this.ordnum = ordnum;
+    }
+
+    public double getOrdamount() {
+        return ordamount;
+    }
+
+    public void setOrdamount(double ordamount) {
+        this.ordamount = ordamount;
+    }
+
+    public double getAdvanceamount() {
+        return advanceamount;
+    }
+
+    public void setAdvanceamount(double advanceamount) {
+        this.advanceamount = advanceamount;
+    }
+
+    public String getOrderdescription() {
+        return orderdescription;
+    }
+
+    public void setOrderdescription(String orderdescription) {
+        this.orderdescription = orderdescription;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 }
